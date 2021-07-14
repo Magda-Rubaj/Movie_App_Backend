2ff778082e499bedfa5dd6870dc90b8ee1ee8fa4
Magda-Rubaj <magiru@o2.pl>
2021-07-14 15:16:31 +0200

    added registration

--- Movie_App/apps/accounts/views.py
+++ Movie_App/apps/accounts/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import EmailTokenSerializer
 
+from rest_framework import generics, viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from .serializers import EmailTokenSerializer, UserSerializer
 
 
 class EmailTokenView(TokenObtainPairView):
     serializer_class = EmailTokenSerializer
+
+class UserView(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
