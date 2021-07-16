e3a19a088bfcff5ee532a38723c6a2be7595e959
Magda-Rubaj <magiru@o2.pl>
2021-07-16 11:09:27 +0200

    added media, additional fields for movie model

--- Movie_App/apps/movies/models.py
+++ Movie_App/apps/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Movie(models.Model):
@@ -6,4 +7,7 @@ class Movie(models.Model):
     production_year= models.IntegerField()
     image = models.ImageField(default='defaults\movie.jpg', upload_to='posted_movies',)
     description = models.TextField()
+    added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
+    rating = models.IntegerField(default=5)
+
 
