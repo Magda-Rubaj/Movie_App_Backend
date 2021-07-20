468a66eade6509f7522e44ef3429cfec72c4d7cd
Magda-Rubaj <magiru@o2.pl>
2021-07-20 17:50:01 +0200

    tested permissions for accounts

--- Movie_App/apps/accounts/permissions.py
+++ Movie_App/apps/accounts/permissions.py
@@ -5,10 +5,12 @@ class IsPostAdminOrSelf(BasePermission):
 
     def has_permission(self, request, view):
         if request.method == 'POST':
-            return True
-        return request.user.is_authenticated and request.user.is_admin
-    
+                return True
+        if view.action == 'list':
+            return request.user.is_authenticated and request.user.is_admin
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and request.user.is_admin:
+        if request.user.is_admin:
             return True
         return obj == request.user
--- Movie_App/apps/accounts/tests.py
+++ Movie_App/apps/accounts/tests.py
@@ -38,21 +38,74 @@ class PostUserTests(APITestCase):
         response = self.view(request)
         self.assertEqual(response.status_code, 400)
     
+
 class GetUserTests(APITestCase):
 
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.user = User.objects.create(email='example@example.com', password='pass')
+
+        self.regular_user = User.objects.create_user(
+            email='example@example.com', 
+            password='pass'
+        )
+
+        self.admin_user = User.objects.create_user(
+            email='admin@example.com', 
+            password='pass', 
+            is_admin=True
+        )
+
         self.view = UserView.as_view(actions={'get': 'retrieve'})
 
-    def test_unauthorized_self(self):
-        request = self.factory.get('/users/{0}/'.format(self.user.id))
+    def test_unauth_self(self):
+        request = self.factory.get('/users/{0}/'.format(self.regular_user.id))
         response = self.view(request)
         self.assertEqual(response.status_code, 401)
 
-    def test_authorized_self(self):
-        request = self.factory.get('/users/{0}/'.format(self.user.id))
-        force_authenticate(request, user=self.user)
-        response = self.view(request, pk=self.user.pk)
+    def test_auth_self(self):
+        request = self.factory.get('/users/{0}/'.format(self.regular_user.id))
+        force_authenticate(request, user=self.regular_user)
+        response = self.view(request, pk=self.regular_user.pk)
         self.assertEqual(response.status_code, 200)
 
+    def test_as_regular_another_user(self):
+        request = self.factory.get('/users/{0}/'.format(self.admin_user.id))
+        force_authenticate(request, user=self.regular_user)
+        response = self.view(request, pk=self.admin_user.pk)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_as_admin_another_user(self):
+        request = self.factory.get('/users/{0}/'.format(self.regular_user.id))
+        force_authenticate(request, user=self.admin_user)
+        response = self.view(request, pk=self.regular_user.pk)
+        self.assertEqual(response.status_code, 200)
+
+
+class GetUsersTests(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+        self.regular_user = User.objects.create_user(
+            email='example@example.com', 
+            password='pass'
+        )
+
+        self.admin_user = User.objects.create_user(
+            email='admin@example.com', 
+            password='pass', 
+            is_admin=True
+        )
+
+        self.view = UserView.as_view(actions={'get': 'list'})
+    
+    def test_as_regular_list(self):
+        request = self.factory.get('/users/')
+        force_authenticate(request, user=self.regular_user)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_as_admin_list(self):
+        request = self.factory.get('/users/')
+        force_authenticate(request, user=self.admin_user)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
